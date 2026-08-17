426bf1043c7aced335b56848845a6f2090ba711e
Gyumin Lee <rbals2626@gmail.com>
2026-08-17 19:09:23 +0900

    comment CRUD API 구현 (CommentController)

--- src/main/java/com/example/board/controller/CommentController.java
+++ src/main/java/com/example/board/controller/CommentController.java
@@ -1,8 +1,13 @@
 package com.example.board.controller;
 
+import com.example.board.domain.Comment;
+import com.example.board.dto.CreateCommentRequest;
+import com.example.board.dto.UpdateCommentRequest;
 import com.example.board.service.CommentService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/comments")
@@ -14,4 +19,18 @@ public class CommentController {
         this.commentService = commentService;
     }
 
+    @PostMapping
+    public Comment createComment(@RequestBody CreateCommentRequest request, Authentication authentication) {
+        return commentService.createComment(request.getPostId(), request.getContent(), authentication.getName());
+    }
+
+    @PutMapping("/{id}")
+    public Comment updateComment(@RequestBody UpdateCommentRequest request, @PathVariable Long id, Authentication authentication) {
+        return commentService.updateComment(request.getContent(), id, authentication.getName());
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteComment(@PathVariable Long id, Authentication authentication) {
+        commentService.deleteComment(id, authentication.getName());
+    }
 }
